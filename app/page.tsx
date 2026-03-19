2be8c4c0fe3c482687142f6e55da7676c10509b7
Gerson Velasco <gerson.velasco@salud.gob.mx>
2026-03-19 14:32:38 -0600

    ajuste para ver login en el home (luego ser resulve como se importa)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import LoginPage from "./login1/page";
+import LoginPage from "@/app/(full-width-pages)/(auth)/login/page";
 
 export default function Home() {
   return (
